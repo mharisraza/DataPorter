c596cebc769b6771986e7047203a539f522cfb4e
Muhammad Haris <wannaheadshot5@gmail.com>
2025-09-02 22:08:56 +0500

    init project

---
+++ src/main/java/dev/dataporter/DataPorterApplication.java
@@ -0,0 +1,13 @@
+package dev.dataporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPorterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dataporter/DataPorterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dataporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
