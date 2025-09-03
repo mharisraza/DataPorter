e721e9052245035ce23d2bb561f64110d5af7090
Muhammad Haris <wannaheadshot5@gmail.com>
2025-09-03 20:58:17 +0500

    unit-test: load and parse cnfg

--- src/test/java/dev/dataporter/DataPorterApplicationTests.java
+++ src/test/java/dev/dataporter/DataPorterApplicationTests.java
@@ -1,13 +1,21 @@
 package dev.dataporter;
 
+import dev.dataporter.core.config.loader.ConfigLoader;
+import dev.dataporter.core.config.model.Config;
+import dev.dataporter.core.config.parser.ConfigParser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 @SpringBootTest
 class DataPorterApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void loadAndParseConfiguration() throws IOException {
+		String loadedRawConfiguration = new ConfigLoader(Path.of("/src/main/resources/static/test.json")).load();
+		Config loadedConfiguration = new ConfigParser(loadedRawConfiguration).parse();
+		assert loadedConfiguration != null && !loadedConfiguration.getHeaderConfigs().isEmpty();
 	}
-
 }
