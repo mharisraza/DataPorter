84b80cf16703c339f18bf95cb5cd68347b3f474a
Muhammad Haris <wannaheadshot5@gmail.com>
2025-09-10 19:08:55 +0500

    added empty rows validation and fixed min row validation

--- src/main/java/dev/dataporter/core/validator/DataSheetValidator.java
+++ src/main/java/dev/dataporter/core/validator/DataSheetValidator.java
@@ -3,28 +3,41 @@ package dev.dataporter.core.validator;
 import dev.dataporter.core.config.model.ConfigGlobalRule;
 import dev.dataporter.core.enums.ErrorSeverity;
 import dev.dataporter.core.enums.ErrorType;
+import dev.dataporter.core.model.data.DataRow;
 import dev.dataporter.core.model.data.DataSheet;
 import dev.dataporter.core.model.validation.ValidationError;
 import dev.dataporter.core.model.validation.ValidationResult;
 
 
-public class ConfigGlobalRuleValidator implements Validator<DataSheet>{
+public class DataSheetValidator implements Validator<DataSheet>{
 
     private final ConfigGlobalRule configGlobalRule;
     private final ValidationResult validationResult;
 
-    public ConfigGlobalRuleValidator(ConfigGlobalRule configGlobalRule, ValidationResult validationFailedResult) {
+    public DataSheetValidator(ConfigGlobalRule configGlobalRule, ValidationResult validationFailedResult) {
         this.configGlobalRule = configGlobalRule;
         this.validationResult = validationFailedResult;
     }
     @Override
-    public void validate(DataSheet dataSet) {
-        validateMinRowsCount(dataSet);
-        validateMaxRowsCount(dataSet);
+    public void validate(DataSheet dataSheet) {
+        validateEmptyRows(dataSheet);
+        validateMinRowsCount(dataSheet);
+        validateMaxRowsCount(dataSheet);
+    }
+
+    private void validateEmptyRows(DataSheet dataSheet) {
+        if(!configGlobalRule.isSkipEmptyRows()) {
+            for (DataRow row : dataSheet.getDataRows()) {
+                if (row.isPhysicallyEmpty()) {
+                    validationResult.addError(new ValidationError("Empty rows are not allowed.", ErrorType.DATA_SHEET, ErrorSeverity.SEVERE));
+                    break;
+                }
+            }
+        }
     }
 
     private void validateMinRowsCount(DataSheet dataSheet) {
-        if(dataSheet.getDataRows().size() < configGlobalRule.getMinRowsCount()) {
+        if(dataSheet.getDataRows().size() - 1 < configGlobalRule.getMinRowsCount()) {
             validationResult.addError(new ValidationError(String.format("Minimum Required rows are %d", configGlobalRule.getMinRowsCount()), ErrorType.DATA_SHEET, ErrorSeverity.SEVERE));
         }
     }
